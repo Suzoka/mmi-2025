afae58b4e7b486a4e3dc86879e1658a316f344a0
Suzoka/Morgan <mor9an77@gmail.com>
2024-10-04 16:33:43 +0200

    cours 2

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    [SerializeField] private float m_Threshold;
+
+    Vector3 m_Origin;
+    CharacterController m_CharacterController;
+
+    private void Awake()
+    {
+        m_CharacterController = GetComponent<CharacterController>();
+    }
+
+    private void Start()
+    {
+        m_Origin = transform.position;
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < m_Threshold)
+        {
+            Debug.Log("Dead");
+            Respawn();
+        }
+    }
+
+    private void Respawn()
+    {
+        m_CharacterController.enabled = false;
+        transform.position = m_Origin;
+        m_CharacterController.enabled = true;
+    }
+}
