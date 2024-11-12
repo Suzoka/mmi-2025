31c4548df14a8517aaf34773b8536a1099e36bb7
Suzoka <mor9an77@gmail.com>
2024-11-12 09:58:09 +0100

    lost commit

--- Assets/Scripts/Approche.cs
+++ Assets/Scripts/Approche.cs
@@ -6,11 +6,27 @@ public class Approche : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
 
+    [SerializeField] Animator m_animator;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == PLAYER_TAG)
+        if (other.gameObject.tag != PLAYER_TAG)
         {
-            Debug.Log("Approche");
+            return;
         }
+
+        Debug.Log("Approche");
+        m_animator.SetBool("IsWithinPlayerRange", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != PLAYER_TAG)
+        {
+            return;
+        }
+
+        Debug.Log("Eloignement");
+        m_animator.SetBool("IsWithinPlayerRange", false);
     }
 }
